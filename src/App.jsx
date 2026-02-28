2330c730d44ebd1e5b24c868376b5960dfade59c
Krish shah <131662091+krishshahhh@users.noreply.github.com>
2026-02-28 17:24:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./component/Header";
-import Main from "./component/main";
+import Main from "./component/Main";
 import Projects from "./component/Projects";
 import Resources from "./component/Resources";
 import Footer from "./component/Footer";
